dbce92f3728c4b69db0094b37adb6627c2fa9400
Sid Roberts <sid@sidroberts.co.uk>
2025-08-10 00:32:44 +0900

    Refactored `Mvc\Model\Resultset\Complex::__serialize()`.

--- src/Mvc/Model/Resultset/Complex.php
+++ src/Mvc/Model/Resultset/Complex.php
@@ -77,15 +77,11 @@ class Complex extends Resultset
          */
         $records = $this->toArray();
 
-        $cache       = $this->cache;
-        $columnTypes = $this->columnTypes;
-        $hydrateMode = $this->hydrateMode;
-
         return [
-            "cache"       => $cache,
+            "cache"       => $this->cache,
             "rows"        => $records,
-            "columnTypes" => $columnTypes,
-            "hydrateMode" => $hydrateMode,
+            "columnTypes" => $this->columnTypes,
+            "hydrateMode" => $this->hydrateMode,
         ];
     }
 
