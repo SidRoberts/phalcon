f7caa4da65e3309c2b29872c31ac528626169d24
Sid Roberts <sid@sidroberts.co.uk>
2025-08-09 13:06:49 +0900

    Refactored `Tag::tagHtml()`.

--- src/Tag.php
+++ src/Tag.php
@@ -1278,12 +1278,11 @@ class Tag
         bool $onlyStart = false,
         bool $useEol = false
     ): string {
-        $params = $parameters;
         if (!is_array($parameters)) {
-            $params = [$parameters];
+            $parameters = [$parameters];
         }
 
-        $localCode = self::renderAttributes("<" . $tagName, $params);
+        $localCode = self::renderAttributes("<" . $tagName, $parameters);
 
         /**
          * Check if Doctype is XHTML
