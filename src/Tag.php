edcee772beb3d4be4370f28cdcabf8d70e9568ef
Sid Roberts <sid@sidroberts.co.uk>
2025-08-09 13:01:17 +0900

    Refactored `Tag::getTitle()`.

--- src/Tag.php
+++ src/Tag.php
@@ -465,18 +465,16 @@ class Tag
     ): string {
         $escaper                = self::getEscaperService();
         $items                  = [];
-        $output                 = "";
         $documentTitle          = $escaper->html(self::$documentTitle);
         $documentTitleSeparator = $escaper->html(self::$documentTitleSeparator);
 
         if (true === $prepend) {
             $documentPrependTitle = self::$documentPrependTitle;
 
-            if (!empty($documentPrependTitle)) {
-                $reverse = array_reverse($documentPrependTitle);
-                foreach ($reverse as $title) {
-                    $items[] = $escaper->html($title);
-                }
+            $reverse = array_reverse($documentPrependTitle);
+
+            foreach ($reverse as $title) {
+                $items[] = $escaper->html($title);
             }
         }
 
@@ -487,10 +485,8 @@ class Tag
         if (true === $append) {
             $documentAppendTitle = self::$documentAppendTitle;
 
-            if (!empty($documentAppendTitle)) {
-                foreach ($documentAppendTitle as $title) {
-                    $items[] = $escaper->html($title);
-                }
+            foreach ($documentAppendTitle as $title) {
+                $items[] = $escaper->html($title);
             }
         }
 
@@ -498,11 +494,7 @@ class Tag
             $documentTitleSeparator = "";
         }
 
-        if (!empty($items)) {
-            $output = implode($documentTitleSeparator, $items);
-        }
-
-        return $output;
+        return implode($documentTitleSeparator, $items);
     }
 
     /**
