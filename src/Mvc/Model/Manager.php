d0d920ec5e899fdb082800211f9537ca4abfed80
Sid Roberts <sid@sidroberts.co.uk>
2025-08-11 16:04:24 +0900

    Refactored `Mvc\Model\Manager::addHasOneThrough()`.

--- src/Mvc/Model/Manager.php
+++ src/Mvc/Model/Manager.php
@@ -693,12 +693,9 @@ class Manager implements ManagerInterface, InjectionAwareInterface, EventsAwareI
         mixed $referencedFields,
         array $options = []
     ): RelationInterface {
-        $entityName         = mb_strtolower(get_class($model));
-        $intermediateEntity = mb_strtolower($intermediateModel);
-        $referencedEntity   = mb_strtolower($referencedModel);
-        $keyRelation        = $entityName . "$" . $referencedEntity;
-
-        $hasOneThrough = $this->hasOneThrough;
+        $entityName       = mb_strtolower(get_class($model));
+        $referencedEntity = mb_strtolower($referencedModel);
+        $keyRelation      = $entityName . "$" . $referencedEntity;
 
         $relations = $this->hasOneThrough[$keyRelation] ?? [];
 
@@ -706,7 +703,7 @@ class Manager implements ManagerInterface, InjectionAwareInterface, EventsAwareI
          * Check if the number of fields are the same from the model to the
          * intermediate model
          */
-        if (is_array($intermediateFields) && count($fields) != count($intermediateFields)) {
+        if (is_array($intermediateFields) && count($fields) !== count($intermediateFields)) {
             throw new ReferencedFieldsMismatch(
                 "HasOneThrough",
                 $entityName,
@@ -718,7 +715,7 @@ class Manager implements ManagerInterface, InjectionAwareInterface, EventsAwareI
          * Check if the number of fields are the same from the intermediate
          * model to the referenced model
          */
-        if (is_array($intermediateReferencedFields) && count($fields) != count($intermediateFields)) {
+        if (is_array($intermediateReferencedFields) && count($fields) !== count($intermediateFields)) {
             throw new ReferencedFieldsMismatch(
                 "HasOneThrough",
                 $entityName,
