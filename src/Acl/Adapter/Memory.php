813c040ee320a4801bc63893327d464bf40e9cf2
Sid Roberts <sid@sidroberts.co.uk>
2025-08-26 12:32:45 +0900

    Refactored `Acl\Adapter\Memory::deny()`.

--- src/Acl/Adapter/Memory.php
+++ src/Acl/Adapter/Memory.php
@@ -480,6 +480,7 @@ class Memory extends AbstractAdapter
         callable | null $function = null
     ): void {
         $rolesArray = [$roleName];
+
         if ('*' === $roleName) {
             $rolesArray = array_keys($this->roles ?? []);
         }
