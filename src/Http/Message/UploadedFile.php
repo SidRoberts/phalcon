8fa5079011c82a8786a382f88ad1c0c3a1a72ce0
Sid Roberts <sid@sidroberts.co.uk>
2025-08-26 14:26:17 +0900

    Refactored `Http\Message\UploadedFile::getErrorDescription()`.

--- src/Http/Message/UploadedFile.php
+++ src/Http/Message/UploadedFile.php
@@ -354,18 +354,17 @@ class UploadedFile implements UploadedFileInterface
      */
     private function getErrorDescription(int $error): string
     {
-        $errors = [
-            0 => "There is no error, the file uploaded with success.",
-            1 => "The uploaded file exceeds the upload_max_filesize directive in php.ini.",
-            2 => "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.",
-            3 => "The uploaded file was only partially uploaded.",
-            4 => "No file was uploaded.",
-            6 => "Missing a temporary folder.",
-            7 => "Failed to write file to disk.",
-            8 => "A PHP extension stopped the file upload.",
-        ];
-
-        return $errors[$error] ?? "Unknown upload error";
+        return match ($error) {
+            0       => "There is no error, the file uploaded with success.",
+            1       => "The uploaded file exceeds the upload_max_filesize directive in php.ini.",
+            2       => "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.",
+            3       => "The uploaded file was only partially uploaded.",
+            4       => "No file was uploaded.",
+            6       => "Missing a temporary folder.",
+            7       => "Failed to write file to disk.",
+            8       => "A PHP extension stopped the file upload.",
+            default => "Unknown upload error",
+        };
     }
 
     /**
