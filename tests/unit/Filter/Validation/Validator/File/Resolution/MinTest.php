0e55779fa77d45a8f499385f9e22f6ab37307fbd
Sid Roberts <sid@sidroberts.co.uk>
2025-09-09 07:09:05 +0900

    Added test classes for `Filter\Validation\Validator\File\Resolution`.

---
+++ tests/unit/Filter/Validation/Validator/File/Resolution/MinTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Filter\Validation\Validator\File\Resolution;
+
+use Phalcon\Tests\AbstractUnitTestCase;
+
+final class MinTest extends AbstractUnitTestCase
+{
+    /**
+     * Tests Phalcon\Filter\Validation\Validator\File\Resolution\Min :: validate
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2025-09-09
+     */
+    public function testFilterValidationValidatorFileResolutionMin(): void
+    {
+        $this->markTestSkipped('Need implementation');
+    }
+}
