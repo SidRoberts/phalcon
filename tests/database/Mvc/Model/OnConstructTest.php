20a588bb62e57ce4ca603b23c1bd5a4caca2d13f
Sid Roberts <sid@sidroberts.co.uk>
2025-09-12 08:03:06 +0900

    Added tests for `Mvc\Model::onConstruct()`.

---
+++ tests/database/Mvc/Model/OnConstructTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Database\Mvc\Model;
+
+use Phalcon\Tests\AbstractDatabaseTestCase;
+use Phalcon\Tests\Support\Traits\DiTrait;
+use Phalcon\Tests\Support\Models\OnConstructModel;
+
+final class OnConstructTest extends AbstractDatabaseTestCase
+{
+    use DiTrait;
+
+    /**
+     * Tests Phalcon\Mvc\Model :: onConstruct()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2025-09-12
+     *
+     * @group mysql
+     */
+    public function testMvcModelConstruct(): void
+    {
+        $this->setNewFactoryDefault();
+
+        $model = new OnConstructModel();
+
+        $this->assertTrue($model->onConstructExecuted);
+    }
+}
