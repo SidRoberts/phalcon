4768cd917c83bef1cd3e963faefcaa50059715d1
Sid Roberts <sid@sidroberts.co.uk>
2025-10-20 08:00:28 +0900

    Reenabled tests for `Crypt::isValidDecryptLength()`.

--- tests/unit/Encryption/Crypt/IsValidDecryptLengthTest.php
+++ tests/unit/Encryption/Crypt/IsValidDecryptLengthTest.php
@@ -13,33 +13,34 @@ declare(strict_types=1);
 
 namespace Phalcon\Tests\Unit\Encryption\Crypt;
 
+use Phalcon\Encryption\Crypt;
 use Phalcon\Tests\AbstractUnitTestCase;
 use Phalcon\Tests\Unit\Encryption\Fake\Crypt\FakeCryptOpensslCipherIvLength;
 
 final class IsValidDecryptLengthTest extends AbstractUnitTestCase
 {
-//    /**
-//     * Tests Phalcon\Encryption\Crypt :: isValidDecryptLength()
-//     *
-//     * @return void
-//     *
-//     * @author Phalcon Team <team@phalcon.io>
-//     * @since  2022-02-09
-//     */
-//    public function testEncryptionCryptGetSetKey(): void
-//    {
-//        $crypt = new Crypt();
-//        $crypt->setKey('1234');
-//
-//        $input = uniqid();
-//        $encrypted = $crypt->encrypt($input);
-//
-//        $actual = $crypt->isValidDecryptLength($encrypted);
-//        $this->assertTrue($actual);
-//
-//        $actual = $crypt->isValidDecryptLength('text');
-//        $this->assertFalse($actual);
-//    }
+    /**
+     * Tests Phalcon\Encryption\Crypt :: isValidDecryptLength()
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2022-02-09
+     */
+    public function testEncryptionCryptGetSetKey(): void
+    {
+        $crypt = new Crypt();
+        $crypt->setKey('1234');
+
+        $input = uniqid();
+        $encrypted = $crypt->encrypt($input);
+
+        $this->assertTrue(
+            $crypt->isValidDecryptLength($encrypted)
+        );
+
+        $this->assertFalse(
+            $crypt->isValidDecryptLength('text')
+        );
+    }
 
     /**
      * @author Phalcon Team <team@phalcon.io>
