763c3aef9f7474510b56cb03634812d1944b08ff
Sid Roberts <sid@sidroberts.co.uk>
2026-05-17 11:18:47 +0900

    Added `DataProvider` attributes to `Flash` tests.

--- tests/unit/Flash/Direct/OutputMessageTest.php
+++ tests/unit/Flash/Direct/OutputMessageTest.php
@@ -16,6 +16,7 @@ namespace Phalcon\Tests\Unit\Flash\Direct;
 use Phalcon\Flash\Direct;
 use Phalcon\Html\Escaper;
 use Phalcon\Tests\AbstractUnitTestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 use const PHP_EOL;
 
@@ -43,11 +44,10 @@ final class OutputMessageTest extends AbstractUnitTestCase
     }
 
     /**
-     * @dataProvider getExamples
-     *
      * @author       Phalcon Team <team@phalcon.io>
      * @since        2020-09-09
      */
+    #[DataProvider('getExamples')]
     public function testFlashDirectOutputMessage(string $type): void
     {
         $flash = new Direct(new Escaper());
