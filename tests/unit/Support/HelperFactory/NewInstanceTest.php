3691fb454c0730f3645e28af77027692faed6034
Sid Roberts <sid@sidroberts.co.uk>
2026-05-17 22:57:14 +0900

    Added `DataProvider` attributes to `Support\HelperFactory` tests.

--- tests/unit/Support/HelperFactory/NewInstanceTest.php
+++ tests/unit/Support/HelperFactory/NewInstanceTest.php
@@ -148,8 +148,6 @@ final class NewInstanceTest extends AbstractUnitTestCase
     }
 
     /**
-     * @dataProvider getExamples
-     *
      * @author       Phalcon Team <team@phalcon.io>
      * @since        2020-09-09
      */
