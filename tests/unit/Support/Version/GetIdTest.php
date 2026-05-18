267b16e9666008fe12b0a3676c9c53800d107644
Sid Roberts <sid@sidroberts.co.uk>
2026-05-18 18:11:08 +0900

    Fixed PHPdoc spacing in `Support\Version` tests.

--- tests/unit/Support/Version/GetIdTest.php
+++ tests/unit/Support/Version/GetIdTest.php
@@ -53,8 +53,8 @@ final class GetIdTest extends AbstractUnitTestCase
     }
 
     /**
-     * @author       Phalcon Team <team@phalcon.io>
-     * @since        2020-09-09
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-09-09
      */
     #[DataProvider('getExamples')]
     public function testSupportVersionGetId(
--- tests/unit/Support/Version/GetTest.php
+++ tests/unit/Support/Version/GetTest.php
@@ -50,8 +50,8 @@ final class GetTest extends AbstractUnitTestCase
     }
 
     /**
-     * @author       Phalcon Team <team@phalcon.io>
-     * @since        2020-09-09
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2020-09-09
      */
     #[DataProvider('getExamples')]
     public function testSupportVersionGet(
